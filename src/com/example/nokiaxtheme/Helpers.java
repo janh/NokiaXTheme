4fa82a94da1ad4167560773aaa4f55c4ada93fe4
Jan Hoffmann <jan.christian.hoffmann@gmail.com>
2014-04-02 01:19:47 +0200

    Initial code commit

---
+++ src/com/example/nokiaxtheme/Helpers.java
@@ -0,0 +1,26 @@
+package com.example.nokiaxtheme;
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.os.Build;
+
+public class Helpers {
+
+	/**
+	 * Sets the correct theme based on the device that the app is running on.
+	 * You have two options for implementing this:
+	 *  - set the theme on the Application object
+	 *  - call this method at the beginning of onCreate in every Activity
+	 * 
+	 * @param c  context, e.g. an Application or Activity
+	 */
+	public static void setTheme(Context c) {
+		//if (!Build.MANUFACTURER.toLowerCase(Locale.US).contains("nokia")) {
+		//	c.setTheme(R.style.AppTheme);
+		//} else {
+			c.setTheme(R.style.AppTheme_Nokia);
+		//}
+	}
+	
+}
