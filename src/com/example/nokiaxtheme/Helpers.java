aa8a3b3ff3e4feb0c56c8626f866988f4914dba4
Jan Hoffmann <jan.christian.hoffmann@gmail.com>
2014-04-04 01:43:52 +0200

    Add light theme variants

--- src/com/example/nokiaxtheme/Helpers.java
+++ src/com/example/nokiaxtheme/Helpers.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.os.Build;
 
 public class Helpers {
-
+	
 	/**
 	 * Sets the correct theme based on the device that the app is running on.
 	 * You have two options for implementing this:
@@ -17,9 +17,13 @@ public class Helpers {
 	 */
 	public static void setTheme(Context c) {
 		if (!Build.MANUFACTURER.toLowerCase(Locale.US).contains("nokia")) {
-			c.setTheme(R.style.AppTheme);
+			//c.setTheme(R.style.AppTheme);
+			//c.setTheme(R.style.AppTheme_Light);
+			c.setTheme(R.style.AppTheme_Light_DarkActionBar);
 		} else {
-			c.setTheme(R.style.AppTheme_Nokia);
+			//c.setTheme(R.style.AppTheme_Nokia);
+			//c.setTheme(R.style.AppTheme_Nokia_Light);
+			c.setTheme(R.style.AppTheme_Nokia_Light_DarkActionBar);
 		}
 	}
 	
